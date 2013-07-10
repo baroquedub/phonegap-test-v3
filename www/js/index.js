793bb197d624942fb94824dca9a48b5cfe94486b
baroquedub <baroquedub@googlemail.com>
2013-07-10 15:40:34 +0100

    Revert "added new htm"
    This reverts commit 57f7750d68d02e42f6a13e09ee6dd9bd201042dd.

--- www/js/index.js
+++ www/js/index.js
@@ -33,6 +33,17 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-       window.location.href = "./hm-1100-100/index.html";
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
     }
 };
